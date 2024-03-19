3f02f430de93bf301ab1eff10c49141a78e6c19d
Derrick Ankrah <darhin97@gmail.com>
2024-03-19 23:58:02 +0000

    initial

--- app/(site)/_components/auth-form.tsx
+++ app/(site)/_components/auth-form.tsx
@@ -37,7 +37,17 @@ const AuthForm = () => {
     }
   };
 
-  return <div>Auth form</div>;
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div className={"mt-8 sm:mx-auto sm:w-full sm:max-w-md"}>
+      <div className={"bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10"}>
+        <form className={"space-y-6"} onSubmit={handleSubmit(onSubmit)}></form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
