bcc0034cbf114d0d271d67cf0668fb7623d6a778
Derrick Ankrah <darhin97@gmail.com>
2024-03-20 23:40:28 +0000

    added auth

--- app/(site)/_components/auth-form.tsx
+++ app/(site)/_components/auth-form.tsx
@@ -2,12 +2,18 @@
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Input from "@/app/components/inputs/input";
+import Button from "@/app/components/button";
+import AuthSocialButton from "@/app/(site)/_components/auth-social-button";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>("LOGIN");
-  const [loading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
@@ -29,25 +35,121 @@ const AuthForm = () => {
     },
   });
 
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
+      try {
+        await axios.post("/api/register", data);
+        setIsLoading(false);
+      } catch (e) {
+        toast.error("Something went wrong");
+        setIsLoading(false);
+      }
     }
     if (variant === "LOGIN") {
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials");
+          }
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in!");
+          }
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
     }
   };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
+    signIn(action).then((callback) => {
+      if (callback?.error) {
+        toast.error("Invalid Credentials");
+      }
+      if (callback?.ok && !callback?.error) {
+        toast.error("Logged in!");
+      }
+    });
   };
 
   return (
     <div className={"mt-8 sm:mx-auto sm:w-full sm:max-w-md"}>
       <div className={"bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10"}>
         <form className={"space-y-6"} onSubmit={handleSubmit(onSubmit)}>
-          <Input />
+          {variant === "REGISTER" && (
+            <Input
+              register={register}
+              id={"name"}
+              label={"Name"}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            register={register}
+            id={"email"}
+            label={"Email"}
+            type={"email"}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            register={register}
+            id={"password"}
+            label={"Password"}
+            type={"password"}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type={"submit"}>
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
         </form>
+        <div className={"mt-6"}>
+          <div className={"relative"}>
+            <div className={"absolute inset-0 flex items-center"}>
+              <div className={"w-full border-t border-gray-300"}></div>
+            </div>
+            <div className={"relative flex justify-center text-sm"}>
+              <span className={"bg-white px-2 text-gray-500"}>
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className={"mt-6 flex gap-2"}>
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+        <div
+          className={
+            "flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500 "
+          }
+        >
+          <div>
+            {variant === "LOGIN"
+              ? "New to MessageMe?"
+              : "Already have an account"}
+          </div>
+          <div onClick={toggleVariant} className={"underline cursor-pointer"}>
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ app/api/register/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { db } from "@/libs/prismadb";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { email, password, name } = body;
+
+    if (!email || !name || !password) {
+      return new NextResponse("Missing info", { status: 400 });
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await db.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log(error, "Registration Error");
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
